c2ab2425e9b91ba062e30211d806e58d1c8dad86
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2022-08-04 11:27:38 +0900

    入力した企業名のカレンダーを作成

--- src/code.js
+++ src/code.js
@@ -1,3 +1,13 @@
 function doGet() {
   return HtmlService.createTemplateFromFile("src/index").evaluate();
 }
+
+/**
+ * 企業カレンダー作成
+ * @param {string} enterpriseName 企業名
+ */
+function createEnterpriseCalendar(enterpriseName) {
+  CalendarApp.createCalendar(enterpriseName, {
+    summary: { createdBy: "就活スケジュール管理", aspiration: "middle" },
+  });
+}
